0fde1e87008930efd08178a73ce7fe57b830da56
Fachry Anwar <fachryanwar2926@gmail.com>
2024-06-16 16:08:03 +0700

    feat: book covers history

--- app/src/main/java/com/example/bookmate/ui/bookdetail/BookDetailActivity.kt
+++ app/src/main/java/com/example/bookmate/ui/bookdetail/BookDetailActivity.kt
@@ -1,6 +1,5 @@
 package com.example.bookmate.ui.bookdetail
 
-import android.nfc.NfcAdapter.EXTRA_ID
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
--- app/src/main/java/com/example/bookmate/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/bookmate/ui/profile/ProfileFragment.kt
@@ -2,7 +2,6 @@ package com.example.bookmate.ui.profile
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -52,6 +51,20 @@ class ProfileFragment : Fragment() {
         viewModel.profileData.observe(requireActivity()) {
             binding.tvReadingList.text = it.readingList.toString()
             binding.tvBookReviewed.text = it.listRating.toString()
+
+            if (it.listImage.isEmpty()) {
+                binding.rvHistory.visibility = View.GONE
+                binding.imgNoHistory.visibility = View.VISIBLE
+                binding.tvNoHistory.visibility = View.VISIBLE
+            } else {
+                binding.rvHistory.visibility = View.VISIBLE
+                binding.imgNoHistory.visibility = View.GONE
+                binding.tvNoHistory.visibility = View.GONE
+
+                val adapter = HistoryAdapter()
+                adapter.submitList(it.listImage)
+                binding.rvHistory.adapter = adapter
+            }
         }
     }
 
